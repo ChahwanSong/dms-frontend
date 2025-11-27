099ac11da5e0c53c7d56ff1126807b29310093e6
Chahwan <49546103+ChahwanSong@users.noreply.github.com>
2025-11-28 08:41:39 +0900

    Remove local scheduler/frontend helper scripts

--- local_dms_frontend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eux pipefail;\
-    export DMS_REDIS_WRITE_URL="redis://127.0.0.1:6379";\
-    export DMS_REDIS_READ_URL="redis://127.0.0.1:6379";\
-    export DMS_SCHEDULER_BASE_URL="http://0.0.0.0:9000";\
-    python3 -m uvicorn app.main --host 127.0.0.1 --port 8000
--- local_dms_scheduler.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eux pipefail;\
-    python3 -m uvicorn cli.local_scheduler:app --host 0.0.0.0 --port 9000
-
-
